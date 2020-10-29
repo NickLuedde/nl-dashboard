510ead10eb41a997d5346dad33df0dddab231d7b
NICK LUEDDE <nicholas@luedde.dev>
2020-10-29 14:55:10 -0500

    Add files via upload

--- Host_Header.js
+++ Host_Header.js
@@ -1,42 +1,27 @@
 import React from "react";
 import "./Primary.css";
 
-
-
-
 class HostSelector extends React.Component {
+  render() {
+    return (
+      <div>
+        {/* Host Selector */}
+        <div className="Header">
+          <select className="Host">
+            <p>a graphing solution</p>
+            <option>Host</option>
 
-  
- 
+            <option href="#">logic-test-01</option>
+            <option href="#">logic-test-02</option>
+          </select>
 
-    render() {
-      return (
-       
-       <div>
-         
-          {/* Host Selector */}
-          <div className="Header">
-          
-            <select className="Host">
-              <option>Host</option>
-              
-              <option href="#">logic-test-01</option>
-              <option href="#">logic-test-02</option>
-            </select>
-  
           {/* The Header */}
-      
-         <h1>graphster </h1> 
-         
-            
-          
 
-          </div>
-         
-             </div>
-      );
-    }
+          <h1>graphster </h1>
+        </div>
+      </div>
+    );
   }
-  
-  export default HostSelector;
-  
+}
+
+export default HostSelector;
